26ec79a1f42dd7bb103b7499dba0629e87d96e8b
Bart van der Schoor <bartvanderschoor@gmail.com>
2014-06-04 14:46:00 +0000

    publish gh-pages (auto)
    branch: master
    SHA: 33b089e5934d91cedebad0038a9ae9de17b45356
    range SHA: dc2f3029b28d...33b089e5934d
    build id: 26764880
    build number: 3

--- js/lorez.js
+++ js/lorez.js
@@ -1,8 +1,8 @@
-!function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.lorez=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.lorez=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 'use strict';
 var RGBA = _dereq_('./RGBA');
 
-var microFont = _dereq_('../font/micro');
+var microFont = _dereq_('../font/Micro');
 
 var util = _dereq_('./util');
 var rand = util.rand;
@@ -375,7 +375,7 @@ var Bitmap = (function () {
 module.exports = Bitmap;
 //# sourceMappingURL=Bitmap.js.map
 
-},{"../font/micro":15,"./RGBA":6,"./util":13}],2:[function(_dereq_,module,exports){
+},{"../font/Micro":15,"./RGBA":6,"./util":13}],2:[function(_dereq_,module,exports){
 'use strict';
 var Char = (function () {
     function Char(char, map) {
@@ -1380,7 +1380,7 @@ var font = new Font('micro', 4, {
 });
 
 module.exports = font;
-//# sourceMappingURL=micro.js.map
+//# sourceMappingURL=Micro.js.map
 
 },{"../core/Font":4}],16:[function(_dereq_,module,exports){
 'use strict';
